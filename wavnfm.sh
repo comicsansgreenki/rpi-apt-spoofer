4781c8a554d52fe5905d659d934f7bafdc058b6b
comicsansgreenkirby <59182034+Ben1701A@users.noreply.github.com>
2022-04-28 18:21:49 -0500

    Move temp file location to /tmp + 2 more changes
    0: Moved temporary file location to /tmp/rpi-apt-spoofer.wav
    1: Edited output strings
    2: Added warnings for frequency overlapping NOAA satellites

--- wavnfm.sh
+++ wavnfm.sh
@@ -1,17 +1,42 @@
 #!/bin/sh
-
-#(while true; do cat sampleaudio.wav; done) | csdr convert_i16_f | csdr gain_ff 2500 | sudo ./sendiq -i /dev/stdin -s 24000 -f 434e6 -t float 1
+if [[ "$1" = "137100" ]]; then
+	echo "** WARNING: Transmission frequency overlaps NOAA-19! (input freq: $1)"
+	echo "**          Please make sure that your radio transmissions are well-contained."
+	echo "**          Otherwise, you may be in some *very bad legal doodoo.*"
+	echo "**"
+	echo "**          YOU HAVE BEEN WARNED . . ."
+	echo " "
+fi
+if [[ "$1" = "137620" ]]; then
+	echo "** WARNING: Transmission frequency overlaps NOAA-15! (input freq: $1)"
+	echo "**          Please make sure that your radio transmissions are well-contained."
+	echo "**          Otherwise, you may be in some *very bad legal doodoo.*"
+	echo "**"
+	echo "**          YOU HAVE BEEN WARNED . . ."
+	echo " "
+fi
+if [[ "$1" = "137900" ]]; then
+	echo "** WARNING: Transmission frequency overlaps NOAA-18! (input freq: $1)"
+	echo "**          Please make sure that your radio transmissions are well-contained."
+	echo "**          Otherwise, you may be in some *very bad legal doodoo.*"
+	echo "**"
+	echo "**          YOU HAVE BEEN WARNED . . ."
+	echo " "
+fi
+### Even then, make sure they're well-contained unless you plan on transmitting with a good low-pass filter.
 
 if ( "$3" = "-c" ); then
-	echo "**Converting file**"
-	ffmpeg -i "$2" -ac 1 -ar 48000 -acodec s16_le temp.wav
-        echo "**Transmitting! Ctrl+C to stop before end**"
-	(cat temp.wav) | csdr convert_i16_f \
+	echo "Converting file via ffmpeg ..."
+	ffmpeg -i "$2" -ac 1 -ar 48000 -acodec s16_le /tmp/rpi-apt-spoofer.wav
+        echo "** Transmitting! **"
+	echo "Type ^C to stop."
+	(cat /tmp/rpi-apt-spoofer.wav) | csdr convert_i16_f \
 	  | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 \
 	  | sudo rpitx -i- -m RF -f "$1"
-        rm temp.wav
+        rm /tmp/rpi-apt-spoofer.wav
 else
-	echo "**Transmitting! Ctrl+C to stop before end**"
+	echo "** Transmitting! **"
+	echo "Type ^C to stop."
 	(cat "$2") | csdr convert_i16_f \
           | csdr gain_ff 7000 | csdr convert_f_samplerf 20833 \
           | sudo rpitx -i- -m RF -f "$1"
